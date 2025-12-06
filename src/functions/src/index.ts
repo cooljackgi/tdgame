0af5f877d253f55d3ffdfb27c13b52004889f38f
david becker <db@hudb.de>
2025-12-06 14:22:38 +0000

    die joingame function war super da war nichts falsch dran jetzt bekommt

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -68,15 +68,12 @@ export const joinGame = functions.https.onCall(async (data, context) => {
 
       // If we reach here, P2 slot is free.
       const resources = gameData?.players?.player1?.resources ?? 1250;
-      // CORRECTED: Ensure 'members' is treated as an array.
-      const membersArray = Array.isArray(gameData?.members) ? gameData.members : [];
-      const newMembers = [...new Set([...membersArray, uid])];
-
+      
       // The game status is NOT changed here anymore. It remains 'waiting'.
       // The host will trigger the start of the game from the game screen.
       transaction.update(gameRef, { 
         player2Id: uid, 
-        'members': newMembers, // Use the corrected array
+        'members': [...(gameData?.members || []), uid],
         'players.player2': {
             id: 'player2', 
             name: displayName, 
